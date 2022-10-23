2abc2349349bb8c8888bef141828a5c06786b13f
dr460nf1r3 <root@dr460nf1r3.org>
2022-10-23 09:15:36 +0200

    Actually enable vmware tools

--- nix/garuda/common/esxi.nix
+++ nix/garuda/common/esxi.nix
@@ -1,4 +1,7 @@
 { ... }: {
   # This is on an ESXi, therefore we need the guest tools
-  virtualisation.vmware.guest.headless = true;
+  virtualisation.vmware.guest = {
+    enable = true;
+    headless = true;
+  };
 }
--- nix/garuda/common/lxc.nix
+++ nix/garuda/common/lxc.nix
@@ -1,5 +1,5 @@
 { ... }: {
-  # LXC support
+  # LXC support for Proxmox
   boot.isContainer = true;
   boot.loader.initScript.enable = true;
   systemd.enableUnifiedCgroupHierarchy = false;
