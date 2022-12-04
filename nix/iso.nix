b44511a6bf98b697dd8b63c1369e8101a1685ee8
TNE <tne@garudalinux.org>
2022-12-04 19:56:21 +0100

    update flake lock

--- nix/iso.nix
+++ nix/iso.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ./hardware-configuration.nix
     ./garuda/garuda.nix
@@ -13,6 +13,8 @@
   networking.defaultGateway = "78.129.140.1";
 
   virtualisation.docker.enable = true;
+  environment.systemPackages = with pkgs; [ docker-compose ];
+  networking.firewall.allowedTCPPorts = [ 7777 ];
 
   system.stateVersion = "22.05";
 }
