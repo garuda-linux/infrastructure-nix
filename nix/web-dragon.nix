abd322130c5809ce372421aee7e8efed87b913c6
dr460nf1r3 <root@dr460nf1r3.org>
2023-01-30 20:09:36 +0100

    The new Rust backend for Piped is supposed to bring fixes, lets see!

--- nix/web-dragon.nix
+++ nix/web-dragon.nix
@@ -39,31 +39,6 @@
     source = ./docker-compose/web-dragon;
   };
 
-  # Dirty fix for Piped / Invidious becoming broken after some time
-  systemd.services = {
-    restart-piped = {
-      description = "Automatic restart of the Piped instance";
-      path = [ pkgs.docker ];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = pkgs.writeShellScript "execstart" ''
-          docker restart piped_backend invidious
-        '';
-        wantedBy = [ "multi-user.target" ];
-      };
-    };
-  };
-  systemd.timers = {
-    restart-piped = {
-      description = "Automatic restart of the Piped instance";
-      timerConfig = {
-        OnCalendar = "daily";
-        unit = "restart-piped.service";
-      };
-      wantedBy = [ "timers.target" ];
-    };
-  };
-
   # Reverse proxy for our docker-compose stack
   services.nginx = {
     enable = true;
