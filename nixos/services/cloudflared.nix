7159537f5bed9979e5133b4d54f3a16b577730bc
Nico Jensch <root@dr460nf1r3.org>
2023-10-28 19:16:37 +0200

    fix: bump all inputs, use stable cloudflared since newer one is currently broken

--- nixos/services/cloudflared.nix
+++ nixos/services/cloudflared.nix
@@ -1,10 +1,18 @@
 { config
+, inputs
 , lib
+, pkgs
 , ...
 }:
 with lib;
 let
   cfg = config.services.garuda-cloudflared;
+
+  # https://github.com/cloudflare/cloudflared/issues/1054 - until PR with fix in nixos-unstable
+  # which is this one: https://github.com/NixOS/nixpkgs/pull/263841/files
+  cloudflared = pkgs.callPackage "${toString inputs.nixpkgs}/pkgs/applications/networking/cloudflared" {
+    buildGoModule = pkgs.buildGo120Module;
+  };
 in
 {
   options.services.garuda-cloudflared = {
@@ -16,6 +24,7 @@ in
   config = mkIf cfg.enable {
     services.cloudflared = {
       enable = true;
+      package = cloudflared;
       tunnels.garuda-cloudflared-legacy = {
         inherit (cfg) ingress;
         credentialsFile = cfg.tunnel-credentials;
