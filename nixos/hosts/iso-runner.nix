991c8447728f1ae703c342544af2347a0780455d
Nico Jensch <root@dr460nf1r3.org>
2023-11-11 18:02:36 +0100

    feat: preparing iso-profiles repo ci

--- nixos/hosts/iso-runner.nix
+++ nixos/hosts/iso-runner.nix
@@ -1,15 +1,46 @@
 { lib
+, pkgs
 , sources
 , ...
-}: {
+}:
+let
+  # Simple wrapper to dispatch SSH commands to NixOS
+  ci-trigger = pkgs.writeShellScriptBin "ci-trigger" ''
+    echo $SSH_ORIGINAL_COMMAND
+    _FLAVOUR=$(echo $SSH_ORIGINAL_COMMAND | cut -d' ' -f2)
+
+    case "$SSH_ORIGINAL_COMMAND" in
+      "ci-trigger buildall")
+        echo "Building all ISO Garuda currently offers.."
+        docker exec buildiso buildall || exit 1
+        ;;
+      "ci-trigger "* )
+        echo "Building $_FLAVOUR.."
+        docker exec buildiso buildiso -i || exit 2
+        docker exec buildiso buildiso -p $_FLAVOUR || exit 3
+        ;;
+      *)
+        echo "Access only allowed for building purposes!"
+        exit 666
+    esac
+  '';
+in
+{
   imports = sources.defaultModules ++ [ ../modules ];
 
   # Lets build Garuda ISO here, serving is done via
-  # Temeraire already 
+  # Temeraire already
   services = {
     garuda-iso.enable = true;
     nginx.enable = lib.mkForce false;
     rsyncd.enable = lib.mkForce false;
+  };
+
+  # Create a locked down user for GitLab CI who can only access our wrapper
+  users.users.gitlab = {
+    extraGroups = [ "docker" ];
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [ "restrict,pty,command=\"${ci-trigger}/bin/ci-trigger\"  ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN7W5KtNH5nsjIHBN1zBwEc0BZMhg6HfFurMIJoWf39p" ];
   };
 
   # Let maintainers use buildiso (which is a wrapper around the Docker container)
