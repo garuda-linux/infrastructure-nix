63deb35a6044e73c133a74bd5ea747c6824419b1
Nico Jensch <root@dr460nf1r3.org>
2023-11-13 19:33:52 +0100

    feat: enable NAT reflection for iso-runner, so our own GitLab runner can be used for ISO builds as well

--- nixos/hosts/immortalis.nix
+++ nixos/hosts/immortalis.nix
@@ -113,6 +113,12 @@
         proto = "tcp";
         sourcePort = 223;
       }
+      {
+        destination = "10.0.5.40:22";
+        loopbackIPs = [ "116.202.208.112" ];
+        proto = "tcp";
+        sourcePort = 227;
+      }
     ];
     firewall.trustedInterfaces = [ "br0" ];
   };
