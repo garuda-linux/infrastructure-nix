9ae1b7fa0bc53be27fe1dcd50b7702bb3803fec2
Nico Jensch <root@dr460nf1r3.org>
2024-04-06 09:46:32 +0200

    chore(garuda-mail): cleanup no longer necessary fix

--- nixos/hosts/garuda-mail.nix
+++ nixos/hosts/garuda-mail.nix
@@ -162,9 +162,6 @@ in
   # Postmaster alias
   services.postfix.postmasterAlias = "nico@dr460nf1r3.org";
 
-  # https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/issues/275
-  services.dovecot2.sieve.extensions = [ "fileinto" ];
-
   # Web UI
   services.roundcube = {
     enable = true;
