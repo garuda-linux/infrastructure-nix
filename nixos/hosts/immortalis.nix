5a0802d50be60b0f5b75c7ef0946b7a3d3fe1409
TNE <tne@garudalinux.org>
2024-10-17 12:27:52 +0200

    feat(immortalis): Switch to latest nixpkgs kernel

--- nixos/hosts/immortalis.nix
+++ nixos/hosts/immortalis.nix
@@ -11,7 +11,7 @@
 
   # Increase /tmp & /run size to make better use of RAM
   boot = {
-    kernelPackages = pkgs.linuxPackages_6_6;
+    kernelPackages = pkgs.linuxPackages_latest;
     loader.systemd-boot.enable = true;
     runSize = "50%";
     tmp = {
