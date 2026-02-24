8117cf57b21cc7a45d50a295a55d7f4ca73af9be
TNE <tne@garudalinux.org>
2026-02-24 01:25:23 +0100

    fix: Switch to maintained watchtower version

--- nixos/hosts/aerialis/n8n.nix
+++ nixos/hosts/aerialis/n8n.nix
@@ -16,6 +16,11 @@
         N8N_HOST = "n8n.garudalinux.net";
         N8N_EDITOR_BASE_URL = "https://n8n.garudalinux.net";
         WEBHOOK_URL = "https://n8n-webhooks.garudalinux.net";
+        N8N_PUBLIC_API_DISABLED = true;
+        N8N_PUBLIC_API_SWAGGERUI_DISABLED = true;
+        N8N_DIAGNOSTICS_ENABLED = false;
+        N8N_VERSION_NOTIFICATIONS_ENABLED = false;
+        N8N_TEMPLATES_ENABLED = false;
     };
     openFirewall = true;
   };
